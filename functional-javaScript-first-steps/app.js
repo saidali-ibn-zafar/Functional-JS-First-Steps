67fb924a0e5f0bec645655e00ab47f6a0a27c11b
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-05-11 17:42:48 +0200

    Add files via upload

--- functional-javaScript-first-steps/app.js
+++ functional-javaScript-first-steps/app.js
@@ -1,6 +1,8 @@
+// ==============================
 // PURE AND NOT PURE FUNCTIONS
+// ==============================
 
-// not pure function = = = = = = = = = = = = = = = = = =
+// not pure function = = = = = = = = = = = = = = =
 let name = "Saidali";
 function greet() {
   console.log(`Hello, ${name}!`);
@@ -11,7 +13,7 @@ greet(); // Hello, Saidali!
 name = "Umar";
 greet(); // Hello, Umar!
 
-// pure function = = = = = = = = = = = = = = = = = =
+// pure function = = = = = = = = = = = = = = = = =
 function greeting(name) {
   return `Hello, ${name}`;
 }
@@ -19,3 +21,61 @@ function greeting(name) {
 greet("Saidali"); // Hello, Saidali!
 
 greet("Umar"); // Hello, Umar!
+
+// ==============================
+// IMPERATIVE AND FUNCTIONAL
+// ==============================
+
+// imperative = = = = = = = = = = = = = = =
+let name1 = "Ali";
+let greeiting = "Hi";
+
+console.log(`${greeting}, ${name1}`);
+// Hi, Ali
+
+greeting = "Howdy";
+console.log(`${greeting}, ${name}`);
+// Howdy, Ali
+
+// functional = = = = = = = = = = = = = = =
+
+function greeting1(greeting, name) {
+  return `${greeiting}, ${name}!`;
+}
+
+greeting1("Hi", "Umar");
+// Hi, Umar!
+
+greeting1("Howdy", "Umar");
+// Howdy, Umar!
+
+// ==============================
+// SIDE EFFECTS
+// ==============================
+
+let thesis = { name: "Ali", date: 1991 };
+function renameThesis(newName) {
+  thesis.name = newName;
+  console.log("Renamed!");
+}
+
+renameThesis("Saidali-Ibn_Zafar"); // Renamed!
+thesis; // {name: "Saidali-Ibn-Zafar", date: 1991}
+
+// ==============================
+// NO SIDE EFFECTS
+// ==============================
+
+// let thesis = { name: "Ali", date: 1991 };
+function renameThesis1(oldThesis, newName) {
+  return {
+    name: newName,
+    date: oldThesis.date,
+  };
+}
+
+const thesis2 = renameThesis1(thesis, "Saidali");
+thesis; // {name: "Ali", date: 1991}
+thesis2; // {name: "Saidasli", date: 1991}
+
+ 
