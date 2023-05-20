0b00e0c1419796dc00da4f608fffbee4fd305316
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-05-20 12:10:04 +0200

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,56 +1,56 @@
 // iterator function
-// const countToTen = (num = 1) => {
-//   while (num <= 10) {
-//     console.log(num);
-//     num++;
-//   }
-// };
-// countToTen();
+const countToTen = (num = 1) => {
+  while (num <= 10) {
+    console.log(num);
+    num++;
+  }
+};
+countToTen();
 
 // recursive function
-// const recurToTen = (num = 1) => {
-//   if (num > 10) return;
-//   console.log(num);
-//   num++;
-//   recurToTen(num);
-// };
+const recurToTen = (num = 1) => {
+  if (num > 10) return;
+  console.log(num);
+  num++;
+  recurToTen(num);
+};
 
-// recurToTen();
+recurToTen();
 
 // Without Recursion:
-// const fibonacci = (num, array = [0, 1]) => {
-//   while (num > 2) {
-//     const [nextToLast, last] = array.slice(-2);
-//     array.push(nextToLast + last);
-//     num -= 1;
-//   }
-//   return array;
-// };
+const fibonacci = (num, array = [0, 1]) => {
+  while (num > 2) {
+    const [nextToLast, last] = array.slice(-2);
+    array.push(nextToLast + last);
+    num -= 1;
+  }
+  return array;
+};
 
-// fibonacci(12);
+fibonacci(12);
 
 // With Recursion
 
-// const fib = (num, array = [0, 1]) => {
-//   if (num <= 2) return array;
-//   const [nextToLast1, last1] = array.slice(-2);
-//   return fib(num - 1, [...array, nextToLast1 + last1]);
-// };
+const fib = (num, array = [0, 1]) => {
+  if (num <= 2) return array;
+  const [nextToLast1, last1] = array.slice(-2);
+  return fib(num - 1, [...array, nextToLast1 + last1]);
+};
 
-// fib(12);
+fib(12);
 
 // Without Recursion FIBONACCI POSITION;
-// const fibonacciPos = (pos) => {
-//   if (pos <= 1) return pos;
-//   const seq = [0, 1];
-//   for (let i = 2; i <= pos; i++) {
-//     const [nextToLast, last] = seq.slice(-2);
-//     seq.push(nextToLast + last);
-//   }
-//   return seq[pos];
-// };
+const fibonacciPos = (pos) => {
+  if (pos <= 1) return pos;
+  const seq = [0, 1];
+  for (let i = 2; i <= pos; i++) {
+    const [nextToLast, last] = seq.slice(-2);
+    seq.push(nextToLast + last);
+  }
+  return seq[pos];
+};
 
-// console.log(fibonacciPos(7));
+console.log(fibonacciPos(7));
 
 // With Recursion
 const fibPos1 = (pos) => {
@@ -206,4 +206,62 @@ const sumDigit = (n) => n !== 0 ? n % 10 + sumDigit(parseInt(n / 10)) : 0;
 
 ///////////////////////////////////////////
 const fib = (n) => n >= 2 ? fib(n-1) + fib(n-2) : n;
-////////
+///////////////////////////////////////////
+
+// Consider an array of objects representing different products in a shopping cart, like the one shown below:
+
+let cart = [
+  { name: 'Apple', category: 'Fruit', quantity: 10, price: 0.5 },
+  { name: 'Orange', category: 'Fruit', quantity: 5, price: 0.8 },
+  { name: 'Broccoli', category: 'Vegetable', quantity: 2, price: 1.5 },
+  { name: 'Carrot', category: 'Vegetable', quantity: 8, price: 0.2 },
+  { name: 'Chicken', category: 'Meat', quantity: 1, price: 10 }
+];
+
+// Write a JavaScript function that calculates the total cost of all 'Fruit' items in the cart. Please ensure your function adheres to functional programming principles and avoids changing the original 'cart' array.
+
+// const totalCostOfFruits = Array.prototype.reduce.call(cart, (acc, element) => acc + element.quantity * element.price , 0);
+
+const totalCostOfFruits = cart.filter(el => el.category === 'Fruit').reduce((acc, element) => {
+    return acc + element.quantity * element.price 
+}, 0)
+
+// console.log(totalCostOfFruits);
+
+// Write a pure function named sumArray that takes an array of numbers as an input and returns the sum of all the numbers in the array. A pure function should not alter any external state and should always produce the same output given the same input. 
+
+const sumArray = (arr) => {
+    if(arr.length === 0){
+        return 0;
+    }
+    else if(arr.length === 1){
+        return arr[0];
+    }
+    else{
+        return arr[0] + sumArray(arr.slice(1));
+    }
+}
+
+// console.log(sumArray([]));
+
+// Write a function named createAdder that takes one argument, x. This function should return a new function that takes one argument, y, and returns the sum of x and y. 
+
+const createAdder = (x) => {
+    return (y) => {
+        return x + y;
+    }
+}
+
+// Write a function named curry that takes a function and returns a curried version of the function. The curried function should be callable as a chain of functions, each with a single argument. For example, if there was a function add defined by function add(x, y, z) { return x + y + z; }, then curry(add)(1)(2)(3) should return 6.
+
+function add(x, y, z) {
+  return x + y + z;
+}
+
+const curriedAdd = curry(add);
+console.log(curriedAdd(1)(2)(3)); // Output: 6
+
+// immutability
+// mutability
+
+
